31f41e9c74bca2ddf5054ccb835a1166616d4d77
LudvigFriberg <93259401+LudvigFriberg@users.noreply.github.com>
2023-03-14 14:33:08 +0100

    14/3

--- app.rb
+++ app.rb
@@ -5,6 +5,16 @@ require 'bcrypt'
 require 'sinatra/reloader'
 
 get('/') do
-    slim(:index)
+
+    db = SQLite3::Database.new("db/matcho.db")
+    db.results_as_hash = true
+    #id = params[:id].to_i
+    indextable_1 = db.execute("SELECT time_content FROM Time WHERE court_id = 1")
+    indextable_2 = db.execute("SELECT courtname FROM Court WHERE court_id = 1")
+
+    p result
+    slim(:"index",locals:{time_contents:indextable_1,time_contents:indextable_2})
+    
 end
+
 
